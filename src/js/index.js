dc61a23a76028998e6c60cffa781ca0a2f2b2416
kou <huang-chenlin@kayac.com>
2015-11-25 11:35:19 +0900

    add fn: include Css

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,10 @@ const MD_CONF = {breaks: true};
 
 const Vue = require("vue"),
       marked = require("marked").setOptions(MD_CONF),
-      {html: tidyHtml, css: tidyCss} = require('js-beautify'),
+      {html: tidyHtml} = require('js-beautify'),
       Clipboard = require("clipboard");
 
 const DEMO = require("./demo.js");
-
-const tidy = (str) => {
-  let htmlStr = `<html><head><title>利用規約</title></head><body>${marked(str)}</body></html>`
-  return tidyHtml(htmlStr);
-};
 
 Vue.component("modal", {
   template: "#modal_tpl",
@@ -27,13 +22,46 @@ new Vue({
   el: '#app',
   data: {
     input: DEMO,
-    includeCss: false,
-    showModal: false
+    includeCss: true,
+    showModal: false,
+    styles: null
   },
   filters: {
     marked: marked,
-    tidy: tidy,
-  }
+    tidy: function(str) {
+      let styles = this.includeCss ? this.styles : "",
+          htmlStr = `
+        <!Doctype html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <title>利用規約</title>
+            <style>${styles}</style>
+          </head>
+          <body>${marked(str)}</body>
+        </html>
+      `;
+      return tidyHtml(htmlStr);
+    },
+  },
+  created: function() {
+    this.fetchData();
+  },
+  watch: {
+    styles: "fetchData"
+  },
+  methods: {
+    fetchData: function() {
+      let url = $("#ui_css").attr("href");
+      $.get(url).done((data) => {
+        this.styles = data;
+      });
+    },
+  },
 });
 
-const clipboard = new Clipboard("#copy");
+if (navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1) {
+  $("#copy").hide();
+} else {
+  new Clipboard("#copy");
+}
