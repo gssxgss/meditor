7f53fd2f8126743fbf45856a4bf0bd9b727df650
kou <huang-chenlin@kayac.com>
2015-11-30 11:24:26 +0900

    awspublish script

---
+++ gulp/task/publish.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const gulp = require("gulp");
+const awspublish = require("gulp-awspublish")();
+const config = require("../config");
+
+gulp.task("publish", function() {
+
+  let credentials = require(`${config.siteConfig}/aws.json`),
+      publisher = awspublish.create(credentials);
+
+  let headers = {
+    "Cache-Control": "max-age=315360000, no-transform, public"
+  };
+
+  return gulp.src(`${config.dest}/**/*`)
+    .pipe( awspublish.gzip({ext: ".gz"}) )
+    .pipe( publisher.publish(headers) )
+    .pipe( publisher.cache() )
+    .pipe( publisher.sync() )
+    .pipe( awspublish.reporter({states: ['create', 'update', 'delete']}) );
+
+});
